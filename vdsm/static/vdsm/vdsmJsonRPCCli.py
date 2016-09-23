a5d3c77ca4a97d666fbecf1e2e4b8d129e066b9c
Sandro Bonazzola <sbonazzo@redhat.com>
2016-09-23 10:18:46 +0200

    vdsm: fix vdsmJsonRPCCli.py
    rpm:
    unresolved deps: /bin/python
    
    pyflakes:
    vdsmJsonRPCCli.py:7: 'xml.dom.minidom' imported but unused
    
    pep8:
    vdsmJsonRPCCli.py:13:14: E225 missing whitespace around operator
    vdsmJsonRPCCli.py:15:1: E302 expected 2 blank lines, found 1
    vdsmJsonRPCCli.py:22:80: E501 line too long (84 > 79 characters)
    vdsmJsonRPCCli.py:45:9: E225 missing whitespace around operator
    vdsmJsonRPCCli.py:46:1: E302 expected 2 blank lines, found 0
    vdsmJsonRPCCli.py:66:80: E501 line too long (116 > 79 characters)
    vdsmJsonRPCCli.py:67:23: E203 whitespace before ':'
    vdsmJsonRPCCli.py:71:18: E231 missing whitespace after ':'
    vdsmJsonRPCCli.py:78:35: E251 unexpected spaces around keyword /
    parameter equals
    vdsmJsonRPCCli.py:78:37: E251 unexpected spaces around keyword /
    parameter equals
    vdsmJsonRPCCli.py:78:80: E501 line too long (115 > 79 characters)
    vdsmJsonRPCCli.py:79:80: E501 line too long (109 > 79 characters)
    vdsmJsonRPCCli.py:82:62: E703 statement ends with a semicolon
    vdsmJsonRPCCli.py:85:9: E225 missing whitespace around operator
    vdsmJsonRPCCli.py:87:13: E225 missing whitespace around operator
    vdsmJsonRPCCli.py:89:14: E225 missing whitespace around operator
    vdsmJsonRPCCli.py:90:34: E231 missing whitespace after ','
    vdsmJsonRPCCli.py:92:14: E225 missing whitespace around operator
    vdsmJsonRPCCli.py:93:35: E231 missing whitespace after ','
    vdsmJsonRPCCli.py:93:80: E501 line too long (88 > 79 characters)
    vdsmJsonRPCCli.py:103:80: E501 line too long (128 > 79 characters)
    vdsmJsonRPCCli.py:104:80: E501 line too long (93 > 79 characters)
    vdsmJsonRPCCli.py:116:50: E703 statement ends with a semicolon
    vdsmJsonRPCCli.py:117:7: E225 missing whitespace around operator
    vdsmJsonRPCCli.py:131:80: E501 line too long (104 > 79 characters)
    vdsmJsonRPCCli.py:136:80: E501 line too long (109 > 79 characters)
    vdsmJsonRPCCli.py:145:80: E501 line too long (85 > 79 characters)
    vdsmJsonRPCCli.py:160:80: E501 line too long (85 > 79 characters)
    vdsmJsonRPCCli.py:175:15: E225 missing whitespace around operator
    vdsmJsonRPCCli.py:179:80: E501 line too long (85 > 79 characters)
    vdsmJsonRPCCli.py:187:80: E501 line too long (108 > 79 characters)
    vdsmJsonRPCCli.py:198:80: E501 line too long (109 > 79 characters)
    vdsmJsonRPCCli.py:199:80: E501 line too long (80 > 79 characters)
    vdsmJsonRPCCli.py:218:80: E501 line too long (90 > 79 characters)
    vdsmJsonRPCCli.py:219:80: E501 line too long (109 > 79 characters)
    vdsmJsonRPCCli.py:240:39: E231 missing whitespace after ','
    vdsmJsonRPCCli.py:241:38: E231 missing whitespace after ','
    vdsmJsonRPCCli.py:248:45: E231 missing whitespace after ','
    vdsmJsonRPCCli.py:249:46: E231 missing whitespace after ','
    vdsmJsonRPCCli.py:254:42: E231 missing whitespace after ','
    vdsmJsonRPCCli.py:255:42: E231 missing whitespace after ','
    vdsmJsonRPCCli.py:257:80: E501 line too long (129 > 79 characters)
    vdsmJsonRPCCli.py:273:80: E501 line too long (138 > 79 characters)
    vdsmJsonRPCCli.py:275:80: E501 line too long (157 > 79 characters)
    vdsmJsonRPCCli.py:276:80: E501 line too long (84 > 79 characters)
    vdsmJsonRPCCli.py:294:24: E203 whitespace before ':'
    vdsmJsonRPCCli.py:295:34: E203 whitespace before ':'
    vdsmJsonRPCCli.py:296:19: E203 whitespace before ':'
    vdsmJsonRPCCli.py:297:18: E203 whitespace before ':'
    vdsmJsonRPCCli.py:298:18: E203 whitespace before ':'
    vdsmJsonRPCCli.py:299:15: E203 whitespace before ':'
    vdsmJsonRPCCli.py:319:11: E225 missing whitespace around operator
    
    Change-Id: I16bf427dab9f06b9ed543234cae33757e5627403
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- vdsm/static/vdsm/vdsmJsonRPCCli.py
+++ vdsm/static/vdsm/vdsmJsonRPCCli.py
@@ -1,16 +1,17 @@
-#!/bin/python
+#!/usr/bin/python
+
 import sys
 from vdsm.config import config
 from vdsm import jsonrpcvdscli
 import json
-import xml.dom.minidom
 import argparse
 import requests
 import logging
 import copy
 import random
 
-LOG_FILE_NAME='/var/log/vdsm/cockpit-ovirt.log'
+LOG_FILE_NAME = '/var/log/vdsm/cockpit-ovirt.log'
+
 
 def _set_logger():
     logger = logging.getLogger(__name__)
@@ -19,7 +20,10 @@ def _set_logger():
 
     fh = logging.FileHandler(LOG_FILE_NAME)
     fh.setLevel(logging.DEBUG)
-    debug_fmt = logging.Formatter("%(asctime)s %(message)s", "%m/%d/%Y %I:%M:%S %p")
+    debug_fmt = logging.Formatter(
+        "%(asctime)s %(message)s",
+        "%m/%d/%Y %I:%M:%S %p"
+    )
 
     ih = logging.StreamHandler(stream=sys.stdout)
     ih.setLevel(logging.INFO)
@@ -42,7 +46,9 @@ def ensure_jsonrpcvdscli_compatibility():
     jsonrpcvdscli._COMMAND_CONVERTER['shutdown'] = 'VM.shutdown'
 
 
-_service=None
+_service = None
+
+
 def get_vdsm_service():
     global _service
     if _service is None:
@@ -63,34 +69,45 @@ def read_stdin():
 
 
 def build_result(code, message, content=None):
-    logger.debug('build_result, code: {0}, message: "{1}", content: {2}'.format(code, message, content is not None))
-    result = {'status' : {
+    logger.debug(
+        'build_result, code: {0}, message: "{1}", content: {2}'.format(
+            code, message, content is not None
+        )
+    )
+    result = {'status': {
         'code': code,
         'message': message
         },
-        'content':content
+        'content': content
     }
     return result
 
 
 # invoke engine REST API
 # TODO: use SSL certificates
-def http_call(url, method, headers = {'Accept': 'application/json'}, user=None, pwd=None, body=None, verify=False):
-    logger.debug("http_call: '{0}',\n  method:{1}, user:{2}, headers:{3}".format(url, method, user, headers))
+def http_call(url, method, headers={'Accept': 'application/json'},
+              user=None, pwd=None, body=None, verify=False):
+    logger.debug(
+        "http_call: '{0}',\n  method:{1}, user:{2}, headers:{3}".format(
+            url, method, user, headers
+        )
+    )
 
     if method not in ['GET', 'POST']:
-        logger.info('Unsupported method: {0}'.format(method));
+        logger.info('Unsupported method: {0}'.format(method))
         return None
 
-    auth=None
+    auth = None
     if user:
-        auth=(user, pwd)
+        auth = (user, pwd)
 
-    if method=='GET':
-        result = requests.get(url,auth=auth, headers=headers, verify=verify)
+    if method == 'GET':
+        result = requests.get(url, auth=auth, headers=headers, verify=verify)
 
-    if method=='POST':
-        result = requests.post(url,auth=auth, headers=headers, verify=verify, data=body)
+    if method == 'POST':
+        result = requests.post(
+            url, auth=auth, headers=headers, verify=verify, data=body
+        )
 
     logger.debug('http_call status_code: {0}'.format(result.status_code))
     return result
@@ -100,8 +117,14 @@ def get_engine_token():
     logger.debug('get_engine_token() called')
     credentials = read_credentials()
 
-    url = "{0}/sso/oauth/token?grant_type=urn:ovirt:params:oauth:grant-type:http&scope=ovirt-app-api".format(credentials['url'])
-    resp = http_call(url=url, method='GET', user=credentials['user'], pwd=credentials['pwd'])
+    url = (
+        "{0}/sso/oauth/token?"
+        "grant_type=urn:ovirt:params:oauth:grant-type:http"
+        "&scope=ovirt-app-api"
+    ).format(credentials['url'])
+    resp = http_call(
+        url=url, method='GET', user=credentials['user'], pwd=credentials['pwd']
+    )
     if resp.status_code == 200:
         content = json.loads(resp.text)
         if 'access_token' in content:
@@ -113,8 +136,8 @@ def get_engine_token():
 
 
 def read_credentials():
-    logger.debug('Reading token data from stdin');
-    si=read_stdin()
+    logger.debug('Reading token data from stdin')
+    si = read_stdin()
     try:
         credentials = json.loads(si)
         tolog = credentials.copy()
@@ -128,12 +151,18 @@ def read_credentials():
 
 
 def get_default_headers(credentials):
-    headers = {'Accept': 'application/json', 'Authorization': "Bearer {0}".format(credentials['token'])}
+    headers = {
+        'Accept': 'application/json',
+        'Authorization': "Bearer {0}".format(credentials['token'])
+    }
     return headers
 
 
 def get_default_post_headers(credentials):
-    headers = {'Content-Type': 'application/xml', 'Authorization': "Bearer {0}".format(credentials['token'])}
+    headers = {
+        'Content-Type': 'application/xml',
+        'Authorization': "Bearer {0}".format(credentials['token'])
+    }
     return headers
 
 
@@ -142,7 +171,11 @@ def get_all_vms():
     credentials = read_credentials()
 
     url = "{0}/api/{1}".format(credentials['url'], 'vms')
-    resp = http_call(url=url, method='GET', headers=get_default_headers(credentials))
+    resp = http_call(
+        url=url,
+        method='GET',
+        headers=get_default_headers(credentials)
+    )
     if resp.status_code == 200:
         content = json.loads(resp.text)
         # TODO: prune data before transfer
@@ -157,7 +190,11 @@ def get_host(hostId):
     credentials = read_credentials()
 
     url = "{0}/api/{1}/{2}".format(credentials['url'], 'hosts', hostId)
-    resp = http_call(url=url, method='GET', headers=get_default_headers(credentials))
+    resp = http_call(
+        url=url,
+        method='GET',
+        headers=get_default_headers(credentials)
+    )
     if resp.status_code == 200:
         content = json.loads(resp.text)
         return build_result(0, 'Done', content)
@@ -172,11 +209,15 @@ def get_running_host(credentials=None):
         credentials = read_credentials()
 
     with open('/etc/vdsm/vdsm.id', 'r') as myfile:
-        vdsmId=myfile.read().replace('\n', '')
+        vdsmId = myfile.read().replace('\n', '')
     logger.debug("VDSM ID read: {0}".format(vdsmId))
 
     url = "{0}/api/{1}".format(credentials['url'], 'hosts')
-    resp = http_call(url=url, method='GET', headers=get_default_headers(credentials))
+    resp = http_call(
+        url=url,
+        method='GET',
+        headers=get_default_headers(credentials)
+    )
     if resp.status_code == 200:
         content = json.loads(resp.text)
 
@@ -184,7 +225,11 @@ def get_running_host(credentials=None):
             if host['hardware_information']['uuid'] == vdsmId:
                 return build_result(0, 'Done', host)
 
-        return build_result(1, 'Not found', "VDSM ID '{0}' not found in engine's hosts list".format(vdsmId))
+        return build_result(
+            1,
+            'Not found',
+            "VDSM ID '{0}' not found in engine's hosts list".format(vdsmId)
+        )
     return build_result(resp.status_code, resp.text)
 
 
@@ -195,8 +240,14 @@ def run_engine_vm(vmId):
     xml_request = "<action/>"
 
     url = "{0}/api/vms/{1}/start".format(credentials['url'], vmId)
-    resp = http_call(url=url, method='POST', headers=get_default_post_headers(credentials), body=xml_request)
-    # TODO: refactor error handling and returned data (XML vs. JSON, prune data)
+    resp = http_call(
+        url=url,
+        method='POST',
+        headers=get_default_post_headers(credentials),
+        body=xml_request
+    )
+    # TODO: refactor error handling and returned data
+    #       (XML vs. JSON, prune data)
     if resp.status_code == 200:
         content_xml = resp.text
         return build_result(0, 'Done', content_xml)
@@ -215,8 +266,16 @@ def host_to_maintenance():
 
     xml_request = "<action/>"
 
-    url = "{0}/api/hosts/{1}/deactivate".format(credentials['url'], host['content']['id'])
-    resp = http_call(url=url, method='POST', headers=get_default_post_headers(credentials), body=xml_request)
+    url = "{0}/api/hosts/{1}/deactivate".format(
+        credentials['url'],
+        host['content']['id']
+    )
+    resp = http_call(
+        url=url,
+        method='POST',
+        headers=get_default_post_headers(credentials),
+        body=xml_request
+    )
     if resp.status_code == 200:
         content_xml = resp.text
         return build_result(0, 'Done', content_xml)
@@ -237,24 +296,30 @@ def fake_vm(src, offset):
     strOffset = "f{0:05d}".format(offset)
     fake['vmId'] = strOffset + fake['vmId'][len(strOffset):]
 
-    fake['cpuUser'] = random.randint(0,60)
-    fake['cpuSys'] = random.randint(0,40)
+    fake['cpuUser'] = random.randint(0, 60)
+    fake['cpuSys'] = random.randint(0, 40)
 
     MB = 1024 * 1024
 
     if fake['disks']:
         diskName = fake['disks'].keys()[0]
         disk = fake['disks'][diskName]
-        disk['readRate'] = random.randint(MB,MB*60)
-        disk['writeRate'] = random.randint(MB,MB*60)
+        disk['readRate'] = random.randint(MB, MB*60)
+        disk['writeRate'] = random.randint(MB, MB*60)
 
     if fake['network']:
         netName = fake['network'].keys()[0]
         net = fake['network'][netName]
-        net['rxRate'] = random.randint(MB,MB*60)
-        net['txRate'] = random.randint(MB,MB*60)
+        net['rxRate'] = random.randint(MB, MB*60)
+        net['txRate'] = random.randint(MB, MB*60)
 
-    logger.debug("Fake VM created: vmName: '{0}', vmId: '{1}', srcVmId: '{2}'".format(fake['vmName'], fake['vmId'], src['vmId']))
+    logger.debug(
+        "Fake VM created: vmName: '{0}', vmId: '{1}', srcVmId: '{2}'".format(
+            fake['vmName'],
+            fake['vmId'],
+            src['vmId']
+        )
+    )
     return fake
 
 
@@ -270,10 +335,31 @@ def get_all_vm_stats_fake_extend():
 
 
 def parse_args(service):
-    parser = argparse.ArgumentParser(description='Support utility for Cockpit oVirt plugin to invoke VDSM JSON RPC or Engine REST API.\n')
-    parser.add_argument('vdsmCommand', help='VDSM command to be invoked',
-                        choices=['getAllVmStats', 'shutdown', 'destroy', 'restart', 'ping', 'engineBridge', 'getAllVmStatsFakeExtend', 'getVdsCapabilities'])
-    parser.add_argument('vdsmCommandArgs', help='VDSM command arguments', nargs='*')
+    parser = argparse.ArgumentParser(
+        description=(
+            'Support utility for Cockpit oVirt plugin to invoke '
+            'VDSM JSON RPC or Engine REST API.\n'
+        )
+    )
+    parser.add_argument(
+        'vdsmCommand',
+        help='VDSM command to be invoked',
+        choices=[
+            'getAllVmStats',
+            'shutdown',
+            'destroy',
+            'restart',
+            'ping',
+            'engineBridge',
+            'getAllVmStatsFakeExtend',
+            'getVdsCapabilities'
+        ]
+    )
+    parser.add_argument(
+        'vdsmCommandArgs',
+        help='VDSM command arguments',
+        nargs='*'
+    )
     return parser.parse_args()
 
 
@@ -291,12 +377,12 @@ def main():
     }
 
     COMMANDS = {
-        'getAllVmStats' : service.getAllVmStats,
-        'getAllVmStatsFakeExtend' : get_all_vm_stats_fake_extend,
-        'shutdown' : service.shutdown,
-        'destroy' : service.destroy,
-        'restart' : restart_vm,
-        'ping' : service.ping,
+        'getAllVmStats': service.getAllVmStats,
+        'getAllVmStatsFakeExtend': get_all_vm_stats_fake_extend,
+        'shutdown': service.shutdown,
+        'destroy': service.destroy,
+        'restart': restart_vm,
+        'ping': service.ping,
         'getVdsCapabilities': service.getVdsCapabilities
     }
 
@@ -316,5 +402,5 @@ def main():
 
 
 if __name__ == "__main__":
-    logger=_set_logger()
+    logger = _set_logger()
     sys.exit(main())
