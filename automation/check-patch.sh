473f9e7cb73cd2c37f91effd520f8a07b13a037a
Sandro Bonazzola <sbonazzo@redhat.com>
2017-02-17 16:14:26 +0100

    automation: add rpm build test
    avoid to discover package doesn't build
    only after the merge.
    
    Change-Id: I84851d56ca1092a93e98e83694a77b209225e361
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -6,4 +6,14 @@ yum-deprecated clean metadata || yum clean metadata
 yum-deprecated -y install ${dependencies} || yum -y install ${dependencies}
 
 export PATH=/usr/share/ovirt-engine-nodejs/bin:${PATH}
+
+# Test rpm building, avoiding to discover it doesn't build only after merge.
+# Doing this before make check because npm install may add missing deps at
+# rpm level
+./autogen.sh --with-vdsm
+yum-builddep cockpit-ovirt.spec
+make rpm
+
 ./autogen.sh --with-vdsm --with-npm-install && make -j8 check
+
+
