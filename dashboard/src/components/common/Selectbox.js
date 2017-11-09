e936f6dc2c709d598c32fbdb141bc0b83e700db7
Phillip Bailey <phbailey@redhat.com>
2017-11-09 10:36:24 -0500

    wizard: Avoid Selectbox errors from invalid prop
    This patch updates the Selectbox component to set the selected option to
    the first in the list if the 'selectedOption' prop is invalid. This
    prevents an error from being thrown.
    
    Bug-Url: https://bugzilla.redhat.com/1509775
    Change-Id: Ia498ddd11cf4cf169cfa675c8d1822bc0eeaad18
    Signed-off-by: Phillip Bailey <phbailey@redhat.com>

--- dashboard/src/components/common/Selectbox.js
+++ dashboard/src/components/common/Selectbox.js
@@ -1,28 +1,34 @@
 import React from 'react'
 
 const Selectbox = ({optionList, selectedOption, callBack}) => {
-    const options = []
-    let selected = null
+    const options = [];
+    let selected = null;
+
     optionList.forEach(function (option, index) {
         options.push(
             <li value={option.key} key={option.key}
                 onClick={() => callBack(option.key)}><a>{option.title}</a>
             </li>
-        )
+        );
         if (option.key === selectedOption) {
             selected = option
         }
-    }, this)
+    }, this);
+
+    if (selected === null) {
+        selected = optionList[0];
+    }
+
     return (
         <div className="btn-group bootstrap-select dropdown form-control">
             <button className="btn btn-default dropdown-toggle" type="button"
                 data-toggle="dropdown" aria-expanded="false">
                 <span className="pull-left">{selected.title}</span>
-                <span className="caret"></span>
+                <span className="caret" />
             </button>
             <ul className="dropdown-menu">{options}</ul>
         </div>
     )
-}
+};
 
 export default Selectbox
