ab5986f7ae0f78eaaefbb74a0bdc03cf43be27eb
godas <godas@redhat.com>
2017-11-24 16:03:05 +0530

    conf file location changed and taking backup
    gdeploy conf file location changed to
     /var/lib/ovirt-hosted-engine-setup/gdeploy/gdeployConfig.conf
    he-answer file location changed to
     /var/lib/ovirt-hosted-engine-setup/answers/he-answer.conf
    
    Taking each file backup with timestamp
     for each deployment
    
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1415984
    Change-Id: Ife886bb1e941feb6e4e90c22bac1760144cff0b8
    Signed-off-by: godas <godas@redhat.com>

--- dashboard/src/components/gdeploy/constants.js
+++ dashboard/src/components/gdeploy/constants.js
@@ -2,6 +2,6 @@
 export const CONFIG_FILES = {
     gdeployTemplate: '/usr/share/cockpit/ovirt-dashboard/gdeploy-templates/ovirt-gluster-hc.conf',
     heCommonAnsFile: '/usr/share/cockpit/ovirt-dashboard/gdeploy-templates/he-common.conf',
-    gdeployConfigFile: '/tmp/gdeployConfig.conf',
-    heAnsfileFile: '/tmp/he-answer.conf'
+    gdeployConfigFile: '/var/lib/ovirt-hosted-engine-setup/gdeploy/gdeployConfig.conf',
+    heAnsfileFile: '/var/lib/ovirt-hosted-engine-setup/answers/he-answer.conf'
 }
