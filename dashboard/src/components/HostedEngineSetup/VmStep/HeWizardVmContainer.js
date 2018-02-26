ac66884fdfad543c2dc6cc7af19a271a0bbdb635
Phillip Bailey <phbailey@redhat.com>
2018-02-26 09:40:48 -0500

    wizard: Cleanup handling of VM memory defaults
    This patch sets the initial value of the "Memory Size" field to the
    maximum amount of available memory if it is less than the minimum
    recommended amount; otherwise, it sets it to the minimum recommended
    amount.
    
    It also uses the constant for the minimum recommended amount wherever
    possible and refactors the code used to set the defaults to only call
    the defaultProvider methods once per execution.
    
    Change-Id: I5a0876835078ef7c228c072e9ce150738598448a
    Signed-off-by: Phillip Bailey <phbailey@redhat.com>

--- dashboard/src/components/HostedEngineSetup/VmStep/HeWizardVmContainer.js
+++ dashboard/src/components/HostedEngineSetup/VmStep/HeWizardVmContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { checkDns, checkReverseDns } from '../../../helpers/HostedEngineSetupUtil'
 import { getErrorMsgForProperty, validatePropsForUiStage } from '../Validation'
-import { allIntelCpus, amdCpuTypes, configValues, intelCpuTypes, messages, resourceConstants as constants } from '../constants'
+import { allIntelCpus, amdCpuTypes, configValues, intelCpuTypes, messages, resourceConstants } from '../constants'
 import HeWizardVm from './HeWizardVm'
 
 const defaultAppliances = [
@@ -115,10 +115,14 @@ class HeWizardVmContainer extends Component {
         const defaultsProvider = this.props.defaultsProvider;
         const heSetupModel = this.state.heSetupModel;
 
-        heSetupModel.vm.vmVCpus.value = defaultsProvider.getMaxVCpus() > 4 ?
-            4 : defaultsProvider.getMaxVCpus() ;
-        heSetupModel.vm.vmVCpus.range.max = defaultsProvider.getMaxVCpus();
-        heSetupModel.vm.vmMemSizeMB.range.max = defaultsProvider.getMaxMemAvailable();
+        const maxVCpus = defaultsProvider.getMaxVCpus();
+        heSetupModel.vm.vmVCpus.value = maxVCpus > 4 ? 4 : maxVCpus;
+        heSetupModel.vm.vmVCpus.range.max = maxVCpus;
+
+        const maxMemAvail = defaultsProvider.getMaxMemAvailable();
+        const minRecVmMem = resourceConstants.VM_MEM_MIN_RECOMMENDED_MB;
+        heSetupModel.vm.vmMemSizeMB.range.max = maxMemAvail;
+        heSetupModel.vm.vmMemSizeMB.value = maxMemAvail < minRecVmMem ? maxMemAvail : minRecVmMem;
 
         this.setState({ heSetupModel });
     }
