98f6491ca4f1caef8b2981c1fcf757cba2405415
Sandro Bonazzola <sbonazzo@redhat.com>
2018-03-15 17:37:39 +0100

    automation: re-use build-artifacts.sh
    re-use build-artifacts.sh effectively collecting
    the built rpms so they can be used for manual testing
    after they passes check-patch but before the patch gets
    merged.
    
    Change-Id: I35a587f869cee94d442f1f06512614d1d3ed5359
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,19 +1,10 @@
 #!/bin/bash -xe
 
-# Force CI to get the latest version of these packages:
-dependencies="$(sed -e '/^[ \t]*$/d' -e '/^#/d' automation/packages.force)"
-dnf clean metadata || yum clean metadata
-dnf -y install ${dependencies} || yum -y install ${dependencies}
-
-export PATH=/usr/share/ovirt-engine-nodejs/bin:${PATH}
-
 # Test rpm building, avoiding to discover it doesn't build only after merge.
 # Doing this before make check because npm install may add missing deps at
 # rpm level
-./autogen.sh
-yum-builddep cockpit-ovirt.spec
-make rpm
+./automation/build-artifacts.sh
 
+export PATH=/usr/share/ovirt-engine-nodejs/bin:${PATH}
 ./autogen.sh --with-npm-install && make -j8 check
-
 
