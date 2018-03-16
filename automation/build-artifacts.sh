dfab6e6b9e3e9b2ba380f49fba8082423b89c742
Sandro Bonazzola <sbonazzo@redhat.com>
2018-03-16 11:11:40 +0100

    automation: build timestamped rpms
    build with suffixes when not tagged
    
    Change-Id: I61cd7c2eb38089f5ec22b0c69bc352f0b2bc800d
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>
    (cherry picked from commit 07c82abd7a2dbc90838446a89901107a6e7f247b)

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -1,6 +1,17 @@
 #!/bin/bash -xe
 
 shopt -s nullglob
+# Is this a release - build from tag?
+# tags are like:     cockpit-ovirt-0.11.17-1
+# non tags are like: cockpit-ovirt-0.11.17-1-3-gf36b431
+if git describe --tags --match "cockpit-ovirt*"|cut -f4- -d\-| grep -q '-'; then
+    # This is a master build, we want to make every build
+    # newer than all the previous builds using a timestamp,
+    # and make it easy to locate the commit from the build
+    # with the git commit hash.
+    export PACKAGE_RPM_RELEASE=0
+    export RELEASE_SUFFIX=".$(date --utc +%Y%m%d).git$(git rev-parse --short HEAD)"
+fi
 
 dependencies="$(sed -e '/^[ \t]*$/d' -e '/^#/d' automation/packages.force)"
 dnf clean metadata || yum clean metadata
