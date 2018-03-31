48b06d399b8ded53b7a90c83a9dd32f7f81ffe5f
Phillip Bailey <phbailey@redhat.com>
2018-03-31 09:16:49 -0400

    wizard: Add a password component with unmask option
    This patch adds a password component that allows the user to click an
    icon inside the entry box to unmask the password. A password
    confirmation field is not required when using this component.
    
    Change-Id: I3f05787646cbfed1fcd8739081f9f80986caf2ab
    Signed-off-by: Phillip Bailey <phbailey@redhat.com>

---
+++ dashboard/src/components/HostedEngineSetup/UnmaskablePassword.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+
+const masked = "password-mask-icon fa fa-eye";
+const unmasked = "password-mask-icon fa fa-eye-slash";
+
+class UnmaskablePasswordContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            maskIconClasses: masked,
+            inputType: "password",
+            value: props.value
+        };
+
+        this.toggleMask = this.toggleMask.bind(this);
+    }
+
+    toggleMask () {
+        const iconClasses = this.state.maskIconClasses === masked ? unmasked : masked;
+        const type = this.state.inputType === "password" ? "text" : "password";
+        this.setState({ maskIconClasses: iconClasses, inputType: type });
+    }
+
+    changeHandler(e) {
+        this.props.onChangeHandler(e.target.value);
+    }
+
+    render() {
+        return (
+            <span>
+                <input type={this.state.inputType}
+                       value={this.props.value}
+                       onChange={e => this.changeHandler(e)}
+                       className="form-control" />
+
+                <a onClick={() => this.toggleMask()}>
+                    <i className={this.state.maskIconClasses} aria-hidden="true" />
+                </a>
+            </span>
+        )
+
+    }
+}
+
+UnmaskablePasswordContainer.propTypes = {
+    value: React.PropTypes.string.isRequired,
+    onChangeHandler: React.PropTypes.func.isRequired
+};
+
+export default UnmaskablePasswordContainer;
