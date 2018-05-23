43e9e21c8046332a4bcb4daadc89198e91fd319b
Phillip Bailey <phbailey@redhat.com>
2018-05-23 11:12:51 -0400

    wizard: Disable dirty iSCSI & FC LUNs
    This patch disables dirty iSCSI and fiber channel LUNs and displays a
    warning icon with a tooltip describing the reason why it is disabled.
    
    Change-Id: I37cc6679929f4d53b995ad022443e05febc0bd19
    Bug-Url: https://bugzilla.redhat.com/1576472
    Signed-off-by: Phillip Bailey <phbailey@redhat.com>
    (cherry picked from commit 28bf0c34f832d305e0ff7342ead4f33f3cb6a5c9)

--- dashboard/src/components/HostedEngineSetup/StorageStep/iSCSI/Lun/Lun.js
+++ dashboard/src/components/HostedEngineSetup/StorageStep/iSCSI/Lun/Lun.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { messages, resourceConstants } from "../../../constants";
 
+const lunStatus = {
+    USED: "used",
+    FREE: "free"
+};
+
 const Lun = ({handleLunSelection, lun, selectedLun, storageConfig}) => {
 
     function bytesToGiB(bytes) {
@@ -10,22 +15,34 @@ const Lun = ({handleLunSelection, lun, selectedLun, storageConfig}) => {
     const minLunSizeInGiB = storageConfig.imgSizeGB.value + resourceConstants.LUN_STORAGE_OVERHEAD_GIB;
     const lunSizeInGiB = bytesToGiB(lun.size);
     const lunTooSmall = lunSizeInGiB < minLunSizeInGiB;
+    const lunDirty = lun.status === lunStatus.USED;
+    const disableLun = lunTooSmall || lunDirty;
+
+    const disabledMsgArr = [];
+    if (lunTooSmall) {
+        disabledMsgArr.push(messages.LUN_IS_TOO_SMALL);
+    }
+    if (lunDirty) {
+        disabledMsgArr.push(messages.LUN_IS_DIRTY);
+    }
+    const disabledMsg = disabledMsgArr.join("\n\n");
 
     return (
         <div className="form-group">
             <div className="col-md-12 lun">
                 <div>
-                    {lunTooSmall &&
-                        <i className="pficon pficon-warning-triangle-o he-warning-icon lun-size-warning-icon"
+                    {disableLun &&
+                        <i className="pficon pficon-warning-triangle-o he-warning-icon disabled-lun-warning-icon"
                            rel="tooltip"
-                           title={messages.LUN_IS_TOO_SMALL} />
+                           title={ disabledMsg } />
                     }
+
                     <input type="radio"
                            name="lun"
-                           value={lun.guid}
-                           disabled={lunTooSmall}
-                           checked={selectedLun === lun.guid}
-                           onChange={(e) => handleLunSelection(e.target.value)} />
+                           value={ lun.guid }
+                           disabled={ disableLun }
+                           checked={ selectedLun === lun.guid }
+                           onChange={ (e) => handleLunSelection(e.target.value) } />
                     &nbsp;<span className="lun-prop-label">ID:</span> { lun.guid }
                 </div>
                 <div className="lun-prop"><span className="lun-prop-label">Size (GiB):</span> { lunSizeInGiB.toFixed(2) }</div>
