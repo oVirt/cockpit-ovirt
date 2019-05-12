0b16f7d0951bd0e4a758263194cfc2fe13c0d759
Ido Rosenzwig <irosenzw@redhat.com>
2019-05-12 17:35:57 +0300

    Reserve small amount of memory for the host during deployment
    Reduce the maximum amount of memory the user can
    assign to the engine VM during deployment.
    
    Change-Id: I8c6494ead7cd988048cc57700129f5eddad1aafb
    Bug-Url: https://bugzilla.redhat.com/1709015
    Signed-off-by: Ido Rosenzwig <irosenzw@redhat.com>

--- dashboard/src/components/HostedEngineSetup/constants.js
+++ dashboard/src/components/HostedEngineSetup/constants.js
@@ -112,7 +112,7 @@ export const deploymentOption = {
 };
 
 export const resourceConstants = {
-    VDSM_HOST_OVERHEAD_MB: 256,
+    VDSM_HOST_OVERHEAD_MB: 512,
     VDSM_VM_OVERHEAD_MB: 64,
     VM_MEM_MIN_MB: 2048,
     VM_MEM_MIN_RECOMMENDED_MB: 16348,
