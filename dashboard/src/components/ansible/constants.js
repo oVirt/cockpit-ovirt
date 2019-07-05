1a0f16b7758aad1ed1bc7d9d7dcba80251245798
Gobinda Das <godas@redhat.com>
2019-07-05 12:45:57 +0530

    Option to cleanup deployment in failed deployment
     In case of deployment faliure, there will be option
    from UI to cleanup existing PVs,LVs,VGs etc.
    
    Change-Id: Ibd699cf56d17b7c08c4a692e27d0b0318ba89f0b
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1721371
    Signed-off-by: Gobinda Das <godas@redhat.com>

--- dashboard/src/components/ansible/constants.js
+++ dashboard/src/components/ansible/constants.js
@@ -10,5 +10,7 @@ export const CONFIG_FILES = {
     "performance.low-prio-threads": "32", "performance.strict-o-direct": "on","network.remote-dio": "off", "network.ping-timeout": "30",
     "user.cifs": "off", "nfs.disable": "on", "performance.quick-read": "off", "performance.read-ahead": "off", "performance.io-cache": "off",
     "cluster.eager-lock": "enable"},
-    glusterDeploymentLog: "/var/log/cockpit/ovirt-dashboard/gluster-deployment.log"
+    glusterDeploymentLog: "/var/log/cockpit/ovirt-dashboard/gluster-deployment.log",
+    glusterCleanupPlayBook: '/etc/ansible/roles/gluster.ansible/playbooks/hc-ansible-deployment/tasks/gluster_cleanup.yml',
+    glusterDeploymentCleanUpLog: "/var/log/cockpit/ovirt-dashboard/gluster-deployment_cleanup.log",
 }
