a7b8f78567a10694668bfa8af0fa51d2ef03b1f1
Yedidyah Bar David <didi@redhat.com>
2019-12-04 09:30:28 +0200

    wizard: Require 4096MB for engine VM
    ovirt-ansible-hosted-engine-setup requires 4096MB. Align with it.
    Without this, if setting e.g. 3072, it fails with:
    
    [ ERROR ] fatal: [localhost]: FAILED! => {"changed": false, "msg":
    "Memory size must be at least 4096MB, while you selected only 3072MB"}
    
    Change-Id: I1703108fd209be614a69cb2c4f5a75b4815fc1b5
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- dashboard/src/components/HostedEngineSetup/constants.js
+++ dashboard/src/components/HostedEngineSetup/constants.js
@@ -114,7 +114,7 @@ export const deploymentOption = {
 export const resourceConstants = {
     VDSM_HOST_OVERHEAD_MB: 512,
     VDSM_VM_OVERHEAD_MB: 64,
-    VM_MEM_MIN_MB: 2048,
+    VM_MEM_MIN_MB: 4096,
     VM_MEM_MIN_RECOMMENDED_MB: 16384,
     VM_DISK_MIN_GB: 58,
     VM_DISK_MAX_GB: 4096,
